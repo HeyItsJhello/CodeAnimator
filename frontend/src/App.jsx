2d216da2a03321b7e118d7bd4dae4add4b1f3373
Jhello <105955997+HeyItsJhello@users.noreply.github.com>
2026-01-26 10:34:20 -0800

    Wrap routes with BrowserRouter in App.jsx
    Added BrowserRouter to wrap the Routes component, ensuring proper routing functionality in the application.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import { Routes, Route } from 'react-router-dom'
-import LandingPage from './pages/LandingPage'
-import AppPage from './pages/AppPage'
-import './App.css'
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import AppPage from "./pages/AppPage";
+import "./App.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/app" element={<AppPage />} />
-    </Routes>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/app" element={<AppPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
