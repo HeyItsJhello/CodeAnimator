41354fccb0b85161479a840e1f28e13e5347118d
Jhello <105955997+HeyItsJhello@users.noreply.github.com>
2026-01-26 10:38:23 -0800

    Fix duplicate BrowserRouter wrapping

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,14 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import AppPage from "./pages/AppPage";
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/app" element={<AppPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/app" element={<AppPage />} />
+    </Routes>
   );
 }
 
